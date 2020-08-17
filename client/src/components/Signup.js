425828799eb5827dd22fd47d1a642ba086b29133
maryamradd <maryam.h.alraddadi@gmail.com>
2020-08-18 02:13:30 +0300

    fixed button clicks

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -60,7 +60,7 @@ const Signup = (props) => {
             </Link>
           </p>
         </div>
-        <form className="mt-8" onSubmit={onSubmit}>
+        <form className="mt-8" onSubmit={(event) => onSubmit}>
           <input type="hidden" name="remember" value="true" />
           <div className="rounded-md shadow-sm">
             <div>
@@ -69,7 +69,7 @@ const Signup = (props) => {
                 name="username"
                 type="text"
                 required
-                onChange={onChange}
+                onChange={() => onChange}
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
                 placeholder="Username"
               />
@@ -91,7 +91,7 @@ const Signup = (props) => {
                 name="password"
                 type="password"
                 required
-                onChange={onChange}
+                onChange={() => onChange}
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
                 placeholder="Password"
               />
