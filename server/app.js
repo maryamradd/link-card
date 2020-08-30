7dc4d1e3a6419e6efc5eee6db62f7234709668b7
maryamradd <maryam.h.alraddadi@gmail.com>
2020-08-31 01:27:42 +0300

    cleanup and rewritten to use enviroment variables

--- server/app.js
+++ server/app.js
@@ -1,29 +1,42 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
+// express setup
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
+// routers
 const userRouter = require("./routes/user");
-const dashboardRouter = require("./routes/dashboard");
 
+//load environment variables
+const hostname = process.env.HOST;
+const port = process.env.PORT;
+
+// mongoose setup
 mongoose.connect(
-  "mongodb://localhost:27017/linkcard-app",
+  process.env.MONGODB_CONNECTION_STRING,
   {
+    //to remove mongoose deprecation warnings
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
-  () => {
-    console.log("connected to db successfully");
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to mongoDB successfully");
   }
 );
 
-app.listen(5000, () => {
-  console.log("connected to server successfully");
+// start the server
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+// routers setup
 app.use("/", userRouter);
 
 module.exports = app;
